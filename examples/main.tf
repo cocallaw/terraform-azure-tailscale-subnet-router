7c18f91a9d615e90887e0ff6670b79bd157130bd
Corey Callaway <coreylcallaway@gmail.com>
2022-08-18 08:38:14 -0400

    Create Examples

---
+++ examples/main.tf
@@ -0,0 +1,18 @@
+module "subnet_router" {
+  source = "./modules/subnet_router"
+
+  region                            = var.region
+  resource_group_name               = var.resource_group_name
+  vnet_name                         = var.vnet
+  subnet_name                       = var.subnet_name
+  storage_account_name              = var.storage_account_name
+  container_name                    = var.container_name
+  container_group_name              = var.container_group_name
+  tailscale_ACR_repository          = var.tailscale_ACR_repository
+  tailscale_image_tag               = var.tailscale_image_tag
+  tailscale_ACR_repository_username = var.tailscale_ACR_repository_username
+  tailscale_ACR_repository_password = var.tailscale_ACR_repository_password
+  tailscale_hostname                = var.tailscale_hostname
+  tailscale_advertise_routes        = var.tailscale_advertise_routes
+  tailscale_auth_key                = var.tailscale_auth_key
+}
